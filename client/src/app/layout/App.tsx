cec26b3e9d104213c3b8ba339eac5a33c2f2910f
Dennis Asplind <skene@hotmail.com>
2023-02-18 08:24:01 +0100

    Fetching basket on app start

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,13 +1,30 @@
 import { Container, CssBaseline } from '@mui/material';
 import createTheme from '@mui/material/styles/createTheme';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Header from './Header';
 import 'react-toastify/dist/ReactToastify.css';
+import { useStoreContext } from '../context/StoreContext';
+import { getCookie } from '../util/util';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
+  const { setBasket } = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then((basket) => setBasket(basket))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
+    }
+  }, [setBasket]);
+
   const [darkMode, setDarkMode] = useState(false);
   const palletType = darkMode ? 'dark' : 'light';
 
@@ -23,6 +40,8 @@ function App() {
   const handleThemeChange = () => {
     setDarkMode(!darkMode);
   };
+
+  if (loading) return <LoadingComponent message='Initialising app...' />;
 
   return (
     <ThemeProvider theme={theme}>
