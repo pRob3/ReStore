e1426d870034f64c19588427b29223c4f86fdc69
Dennis Asplind <skene@hotmail.com>
2023-02-18 12:17:46 +0100

    Add checkout page

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -3,6 +3,7 @@ import AboutPage from '../../features/about/AboutPage';
 import BasketPage from '../../features/basket/BasketPage';
 import Catalog from '../../features/catalog/Catalog';
 import ProductDetails from '../../features/catalog/ProductDetails';
+import CheckoutPage from '../../features/checkout/CheckoutPage';
 import ContactPage from '../../features/contact/ContactPage';
 import HomePage from '../../features/home/HomePage';
 import NotFound from '../errors/NotFound';
@@ -19,9 +20,11 @@ export const router = createBrowserRouter([
       { path: 'catalog', element: <Catalog /> },
       { path: 'catalog/:id', element: <ProductDetails /> },
       { path: 'basket', element: <BasketPage /> },
+      { path: 'checkout', element: <CheckoutPage /> },
 
       { path: 'about', element: <AboutPage /> },
       { path: 'contact', element: <ContactPage /> },
+
       { path: 'server-error', element: <ServerError /> },
       { path: 'not-found', element: <NotFound /> },
       { path: '*', element: <Navigate replace to='not-found' /> },
