90072bd7cb6512c8ffd7ac0551012cdef2257f77
Dennis Asplind <skene@hotmail.com>
2023-02-19 15:29:05 +0100

    Search

--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -4,21 +4,50 @@ import {
   createSlice,
 } from '@reduxjs/toolkit';
 import agent from '../../app/api/agent';
-import { Product } from '../../app/models/product';
+import { Product, ProductParams } from '../../app/models/product';
 import { RootState } from '../../app/store/configureStore';
+
+interface CatalogState {
+  productsLoaded: boolean;
+  filtersLoaded: boolean;
+  status: string;
+  brands: string[];
+  types: string[];
+  productParams: ProductParams;
+}
 
 const productsAdapter = createEntityAdapter<Product>();
 
-export const fetchProductsAsync = createAsyncThunk<Product[]>(
-  'catalog/fetchProductsAsync',
-  async (_, thunkAPI) => {
-    try {
-      return await agent.Catalog.list();
-    } catch (error: any) {
-      return thunkAPI.rejectWithValue({ error: error.data });
-    }
+function getAxiosParams(productParams: ProductParams) {
+  const params = new URLSearchParams();
+  params.append('pageNumber', productParams.pageNumber.toString());
+  params.append('pageSize', productParams.pageSize.toString());
+  params.append('orderBy', productParams.orderBy);
+  if (productParams.searchTerm) {
+    params.append('searchTerm', productParams.searchTerm);
   }
-);
+  if (productParams.brands) {
+    params.append('brands', productParams.brands.toString());
+  }
+  if (productParams.types) {
+    params.append('types', productParams.types.toString());
+  }
+
+  return params;
+}
+
+export const fetchProductsAsync = createAsyncThunk<
+  Product[],
+  void,
+  { state: RootState }
+>('catalog/fetchProductsAsync', async (_, thunkAPI) => {
+  const params = getAxiosParams(thunkAPI.getState().catalog.productParams);
+  try {
+    return await agent.Catalog.list(params);
+  } catch (error: any) {
+    return thunkAPI.rejectWithValue({ error: error.data });
+  }
+});
 
 export const fetchProductAsync = createAsyncThunk<Product, number>(
   'catalog/fetchProductAsync',
@@ -31,13 +60,44 @@ export const fetchProductAsync = createAsyncThunk<Product, number>(
   }
 );
 
+export const fetchFilters = createAsyncThunk(
+  'catalog/fetchFilters',
+  async (_, thunkAPI) => {
+    try {
+      return await agent.Catalog.fetchFilters();
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({ error: error.data });
+    }
+  }
+);
+
+function initParams() {
+  return {
+    pageNumber: 1,
+    pageSize: 6,
+    orderBy: 'name',
+  };
+}
+
 export const catalogSlice = createSlice({
   name: 'catalog',
-  initialState: productsAdapter.getInitialState({
+  initialState: productsAdapter.getInitialState<CatalogState>({
     productsLoaded: false,
+    filtersLoaded: false,
     status: 'idle',
+    brands: [],
+    types: [],
+    productParams: initParams(),
   }),
-  reducers: {},
+  reducers: {
+    setProductParams: (state, action) => {
+      state.productsLoaded = false;
+      state.productParams = { ...state.productParams, ...action.payload };
+    },
+    resetProductParams: (state) => {
+      state.productParams = initParams();
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchProductsAsync.pending, (state, action) => {
       state.status = 'pendingFetchProducts';
@@ -62,9 +122,24 @@ export const catalogSlice = createSlice({
       console.log(action);
       state.status = 'idle';
     });
+    builder.addCase(fetchFilters.pending, (state) => {
+      state.status = 'pendingFetchFilters';
+    });
+    builder.addCase(fetchFilters.fulfilled, (state, action) => {
+      state.brands = action.payload.brands;
+      state.types = action.payload.types;
+      state.status = 'idle';
+      state.filtersLoaded = true;
+    });
+    builder.addCase(fetchFilters.rejected, (state, action) => {
+      console.log(action);
+      state.status = 'idle';
+    });
   },
 });
 
 export const productSelectors = productsAdapter.getSelectors(
   (state: RootState) => state.catalog
 );
+
+export const { setProductParams, resetProductParams } = catalogSlice.actions;
