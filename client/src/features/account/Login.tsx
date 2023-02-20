0f974533bef8af56ffe01cd81f7a5d89ac430290
Dennis Asplind <skene@hotmail.com>
2023-02-20 10:58:05 +0100

    Using controlled component

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -10,18 +10,26 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
+import agent from '../../app/api/agent';
 
 const theme = createTheme();
 
 export default function Login() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const [values, setValues] = React.useState({
+    username: '',
+    password: '',
+  });
+
+  const handleSubmit = (event: any) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    console.log(values);
+    agent.Account.login(values);
   };
+
+  function handleInputChange(event: any) {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -44,23 +52,21 @@ export default function Login() {
         <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
           <TextField
             margin='normal'
-            required
             fullWidth
-            id='email'
-            label='Email Address'
-            name='email'
-            autoComplete='email'
+            label='Username'
+            name='username'
             autoFocus
+            onChange={handleInputChange}
+            value={values.username}
           />
           <TextField
             margin='normal'
-            required
             fullWidth
             name='password'
             label='Password'
             type='password'
-            id='password'
-            autoComplete='current-password'
+            onChange={handleInputChange}
+            value={values.password}
           />
 
           <Button
