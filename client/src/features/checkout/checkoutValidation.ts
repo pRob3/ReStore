0526b422ed7cf37ca887c4792f3238b0c1582ee4
Dennis Asplind <skene@hotmail.com>
2023-02-21 17:48:15 +0100

    Payment form

--- client/src/features/checkout/checkoutValidation.ts
+++ client/src/features/checkout/checkoutValidation.ts
@@ -1,10 +1,17 @@
 import * as yup from 'yup';
 
-export const validationSchema = yup.object({
-  fullName: yup.string().required('Full name is required'),
-  address1: yup.string().required('Address 1 is required'),
-  city: yup.string().required('City is required'),
-  state: yup.string().required('State is required'),
-  zip: yup.string().required('Zip is required'),
-  country: yup.string().required('Country is required'),
-});
+export const validationSchema = [
+  yup.object({
+    fullName: yup.string().required('Full name is required'),
+    address1: yup.string().required('Address 1 is required'),
+    city: yup.string().required('City is required'),
+    state: yup.string().required('State is required'),
+    zip: yup.string().required('Zip is required'),
+    country: yup.string().required('Country is required'),
+  }),
+
+  yup.object(),
+  yup.object({
+    nameOnCard: yup.string().required('Name on card is required'),
+  }),
+];
